6985d5b173382dc8addf50b38f8167a3bc589d62
bouzuya <m@bouzuya.net>
2018-03-28 09:06:58 +0900

    use DeepPartial

--- src/get-current-position.ts
+++ src/get-current-position.ts
@@ -1,28 +1,6 @@
-import { AccuracyOptions, PositionOptions } from './position-options';
-
-export interface GetCurrentPositionOptions extends Partial<PositionOptions> {
-
-  // `navigator.geolocation.getCurrentPosition` options
-
-  // enableHighAccuracy default: false. See: PositionOptions.enableHighAccuracy
-  // maximumAge default: 0. See: PositionOptions.maximumAge
-  // timeout default: Infinity. See: PositionOptions.timeout
-
-  // extra options
-
-  // accuracyOptions
-  //   maximumAccuracy default: Infinity.
-  //   minimumTimestamp default: 0.
-  accuracyOptions?: Partial<AccuracyOptions>;
-  // maximumRetryCount default: 0.
-  maximumRetryCount?: number;
-  // `navigator.geolocation.getCurrentPosition` arguments on retry.
-  // retryArguments default: [].
-  //   enableHighAccuracy default: GetCurrentPositionOptions.enableHighAccuracy.
-  //   maximumAge default: GetCurrentPositionOptions.maximumAge.
-  //   timeout default: GetCurrentPositionOptions.timeout.
-  retryArguments?: PositionOptions[];
-}
+import {
+  DeepPartial, GetCurrentPositionOptions, NewPositionOptions
+} from './position-options';
 
 // interface Coordinates {
 //   readonly accuracy: number;
@@ -45,4 +23,7 @@ const getCurrentPosition = (
   return Promise.reject(new Error('Not Implemented Yet'));
 };
 
-export { getCurrentPosition };
+export {
+  DeepPartial, GetCurrentPositionOptions, NewPositionOptions,
+  getCurrentPosition
+};
--- src/position-options.ts
+++ src/position-options.ts
@@ -15,3 +15,22 @@ export interface NewPositionOptions extends PositionOptions {
   maximumRetryCount: number;
   retryArguments: PositionOptions[];
 }
+
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
+
+// `geolocation.PositionOptions` options
+//   enableHighAccuracy default: false. See: PositionOptions.enableHighAccuracy
+//   maximumAge default: 0. See: PositionOptions.maximumAge
+//   timeout default: Infinity. See: PositionOptions.timeout
+// extra options
+//   accuracyOptions
+//     maximumAccuracy default: Infinity.
+//     minimumTimestamp default: 0.
+//   maximumRetryCount default: 0.
+//   retryArguments default: [].
+//     enableHighAccuracy default: GetCurrentPositionOptions.enableHighAccuracy.
+//     maximumAge default: GetCurrentPositionOptions.maximumAge.
+//     timeout default: GetCurrentPositionOptions.timeout.
+export type GetCurrentPositionOptions = DeepPartial<NewPositionOptions>;
