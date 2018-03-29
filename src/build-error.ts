e97852b8d69dca99c84af60199acb70a919fa0bf
bouzuya <m@bouzuya.net>
2018-03-29 19:36:57 +0900

    add some build*Error()

--- src/build-error.ts
+++ src/build-error.ts
@@ -1,4 +1,10 @@
-import { PositionNotSupportedError } from './type/position-error';
+import {
+  PositionNotSupportedError,
+  PositionPermissionDeniedError,
+  PositionPositionUnavailableError,
+  PositionTimeoutError,
+  PositionUnknownError
+} from './type/position-error';
 import { StrictPositionOptions } from './type/position-options';
 
 const buildNotSupportedError = (
@@ -10,4 +16,46 @@ const buildNotSupportedError = (
   };
 };
 
-export { buildNotSupportedError };
+const buildPermissionDeniedError = (
+  options: StrictPositionOptions
+): PositionPermissionDeniedError => {
+  return {
+    options,
+    type: 'permission_denied'
+  };
+};
+
+const buildPositionUnavailableError = (
+  options: StrictPositionOptions
+): PositionPositionUnavailableError => {
+  return {
+    options,
+    type: 'position_unavailable'
+  };
+};
+
+const buildTimeoutError = (
+  options: StrictPositionOptions
+): PositionTimeoutError => {
+  return {
+    options,
+    type: 'timeout'
+  };
+};
+
+const buildUnknownError = (
+  options: StrictPositionOptions
+): PositionUnknownError => {
+  return {
+    options,
+    type: 'unknown'
+  };
+};
+
+export {
+  buildNotSupportedError,
+  buildPermissionDeniedError,
+  buildPositionUnavailableError,
+  buildTimeoutError,
+  buildUnknownError
+};
--- test/build-error.ts
+++ test/build-error.ts
@@ -1,15 +1,45 @@
 import { Test, test } from 'beater';
 import assert from 'power-assert';
-import { buildNotSupportedError } from '../src/build-error';
+import {
+  buildNotSupportedError,
+  buildPermissionDeniedError,
+  buildPositionUnavailableError,
+  buildTimeoutError,
+  buildUnknownError
+} from '../src/build-error';
 import { StrictPositionOptions } from '../src/type/position-options';
 
 const category = '/build-error ';
 const tests: Test[] = [
-  test(category, () => {
+  test(category + 'buildNotSupportedError', () => {
     const options = {} as StrictPositionOptions;
     const error = buildNotSupportedError(options);
     assert(error.options === options);
     assert(error.type === 'not_supported');
+  }),
+  test(category + 'buildPermissionDeniedError', () => {
+    const options = {} as StrictPositionOptions;
+    const error = buildPermissionDeniedError(options);
+    assert(error.options === options);
+    assert(error.type === 'permission_denied');
+  }),
+  test(category + 'buildPositionUnavailableError', () => {
+    const options = {} as StrictPositionOptions;
+    const error = buildPositionUnavailableError(options);
+    assert(error.options === options);
+    assert(error.type === 'position_unavailable');
+  }),
+  test(category + 'buildTimeoutError', () => {
+    const options = {} as StrictPositionOptions;
+    const error = buildTimeoutError(options);
+    assert(error.options === options);
+    assert(error.type === 'timeout');
+  }),
+  test(category + 'buildUnknownError', () => {
+    const options = {} as StrictPositionOptions;
+    const error = buildUnknownError(options);
+    assert(error.options === options);
+    assert(error.type === 'unknown');
   })
 ];
 
